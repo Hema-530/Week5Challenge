da56aa2045522945713d3d106770a0a44570bc98
Hema-530 <62099394+Hema-530@users.noreply.github.com>
2021-02-08 16:23:15 +0530

    Update EmpService1Test.java

--- src/test/java/com/week3challenges/week3challenge/service/EmpService1Test.java
+++ src/test/java/com/week3challenges/week3challenge/service/EmpService1Test.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.week3challenges.week3challenge.empentity.Employee;
 import com.week3challenges.week3challenge.emprepository.EmployeeRepository;
